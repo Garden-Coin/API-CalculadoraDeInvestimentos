646b1242aae9db9b2c2fb8f3d001ac12632401d0
Franklin Bitencourt <franklin.bdantas@hotmail.com>
2023-09-27 22:29:33 -0300

    Inicia testes de unidade

---
+++ tests/unit/validators/juros-compostos-request-validator.test.ts
@@ -0,0 +1,23 @@
+import { describe, it } from 'node:test';
+
+describe(
+    "validators",
+    {
+        concurrency: false,
+        only: false,
+        skip: false,
+        timeout: Infinity,
+        todo: true
+    },
+    ()=>{
+        describe(
+            "period",
+            ()=>{
+                it(
+                    "should be bigger than zero",
+                    ()=>{}
+                )
+            }
+        )
+    }
+)
