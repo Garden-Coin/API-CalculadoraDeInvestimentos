6e4e03ce47dabaf34870b9c76224db108d8ca400
Franklin Bitencourt <franklin.bdantas@hotmail.com>
2023-10-03 21:09:44 -0300

    Corrige import do ErrorMiddleware

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import app from './server';
 
+import { errorHandling } from './middlewares/error-handling'
+
 app.use('/health-check', require('./routes/health-check'));
 app.use('/simulacao', require('./routes/simulacao'));
-app.use(require('./middlewares/error-handling'));
+app.use(errorHandling);
--- src/middlewares/error-handling.ts
+++ src/middlewares/error-handling.ts
@@ -1,11 +1,11 @@
 import { Request, Response, NextFunction } from 'express';
 import { ValidationError } from 'express-validation';
 
-module.exports =  
-    function(err: Error, _req: Request, res: Response, _next: NextFunction){
-        if (err instanceof ValidationError) {
-            return res.status(res.statusCode).json(err)
-        }
-
-        return res.status(500).json(err)
+const errorHandling = function (err: Error, _req: Request, res: Response, _next: NextFunction) {
+    if (err instanceof ValidationError) {
+        return res.status(res.statusCode).json(err)
     }
+
+    return res.status(500).json(err)
+};
+export { errorHandling }
