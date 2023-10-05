844d105e187a18367900d01927a9c682b4e37bf4
Franklin Bitencourt <franklin.bdantas@hotmail.com>
2023-10-04 23:24:10 -0300

    Adiciona testes de unidade do middleware de erros

--- tests/unit/middlewares/error-handling.test.ts
+++ tests/unit/middlewares/error-handling.test.ts
@@ -1,34 +1,92 @@
+import { ValidationError } from "express-validation";
+import { Request, Response } from 'express';
+import { errorHandling } from "@src/middlewares/error-handling"
+
 describe(
     "error handling",
     ()=>{
         describe(
             "validation error",
             ()=>{
-                test(
+                it(
                     "should return validation errors",
-                    async ()=>{ }
+                    ()=>{
+                        const error = new ValidationError(
+                            {
+                              params: [],
+                              headers: [],
+                              query: [],
+                              cookies: [],
+                              signedCookies: [],
+                              body: []
+                            },
+                            { }
+                        );
+                        const response = errorHandling(
+                            error, 
+                            jest.mocked<Request>({} as Request), 
+                            jest.mocked<Response>({} as Response), 
+                            jest.fn()
+                        );
+ 
+                        expect(response.json).toBeCalledWith(error);
+                    }
                 );
                 it(
                     "should return http 404 not found",
                     ()=>{
-        
+                        const error = new ValidationError(
+                            {
+                              params: [],
+                              headers: [],
+                              query: [],
+                              cookies: [],
+                              signedCookies: [],
+                              body: []
+                            },
+                            { }
+                        );
+                        const response = errorHandling(
+                            error, 
+                            jest.mocked<Request>({} as Request), 
+                            jest.mocked<Response>({} as Response), 
+                            jest.fn()
+                        );
+
+                        expect(response.statusCode).toBe(404);
                     }
                 );
             }
         );
         describe(
-            "unmapped error",
+            "unhandled error",
             ()=>{
                 it(
                     "should return only the error message",
                     ()=>{
-        
+                        const error = new Error("unhandled error");
+                        const response = errorHandling(
+                            error, 
+                            jest.mocked<Request>({} as Request), 
+                            jest.mocked<Response>({} as Response), 
+                            jest.fn()
+                        );
+
+                        expect(response.json).toBeCalledWith({message: error.message});
                     }
                 );
                 it(
                     "should return http 500 internal error",
                     ()=>{
-        
+                        const error = new Error("unhandled error");
+                        const response = errorHandling(
+                            error, 
+                            jest.mocked<Request>({} as Request), 
+                            jest.mocked<Response>({} as Response), 
+                            jest.fn()
+                        );
+
+                        expect(response.statusCode).toBe(500);
                     }
                 );
             }
