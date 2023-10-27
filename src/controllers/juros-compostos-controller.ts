5739c2c6349b700f0a73bd2f83d1af70065ca95c
Franklin Bitencourt <franklin.bdantas@hotmail.com>
2023-10-27 08:57:17 -0300

    Adds JurosCompostosService module

--- src/controllers/juros-compostos-controller.ts
+++ src/controllers/juros-compostos-controller.ts
@@ -1,10 +1,10 @@
 import { JurosCompostosRequest } from '@src/requests/juros-compostos-request';
-import jurosCompostosService from '@src/services/juros-compostos-service';
+import { JurosCompostosService } from '@src/services';
 import { JurosCompostosResponse } from '@src/responses/juros-compostos-response';
 
 export default {
 	calcular: function (req: JurosCompostosRequest, res: JurosCompostosResponse) {
-		const response = jurosCompostosService.calcular(req.body);
+		const response = JurosCompostosService.calcular(req.body);
 		return res.status(200).json(response);
 	}
 }; 
---
+++ src/services/index.ts
@@ -0,0 +1,5 @@
+import JurosCompostosService from './juros-compostos-service';
+
+export {
+	JurosCompostosService
+};
