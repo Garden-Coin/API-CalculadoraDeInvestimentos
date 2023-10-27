53827953415d4e86b33f2729e04763e8434fd065
Franklin Bitencourt <franklin.bdantas@hotmail.com>
2023-10-27 12:58:48 -0300

    Adds compression library (with no pratical effect whatsoever)

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import { port } from '@config/server';
 import cors from 'cors';
+import compression from 'compression';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(compression());
 app.listen(port, () => `server running on port ${port}`);
 
 export default app;
