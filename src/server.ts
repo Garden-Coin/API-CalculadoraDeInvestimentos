6b7ce66d4f7ce8aee5145fa4702f287004d7dab8
Franklin Bitencourt <franklin.bdantas@hotmail.com>
2023-10-27 13:16:21 -0300

    add fixed request rate limit

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,18 @@ import express from 'express';
 import { port } from '@config/server';
 import cors from 'cors';
 import compression from 'compression';
+import {rateLimit} from 'express-rate-limit';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use(compression());
+app.use(rateLimit({
+	windowMs: 1000,
+	limit: 10,
+	legacyHeaders: false
+}));
 app.listen(port, () => `server running on port ${port}`);
 
 export default app;
