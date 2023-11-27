8fec97007067462af6c4db3146302354c1ffa203
Franklin Bitencourt <franklin.bdantas@hotmail.com>
2023-11-27 18:40:07 -0300

    Corrige teste de unidade para validation handling

--- tests/unit/middlewares/validation-handling.test.ts
+++ tests/unit/middlewares/validation-handling.test.ts
@@ -5,41 +5,55 @@ import validationErrorFactory from '../factories/express/validation-error-factor
 
 describe(
 	'validation handling',
-	() =>{
+	() => {
 		describe(
 			'validation error',
-			()=>{
+			() => {
 				it(
 					'should return validation errors',
-					()=>{
-						const error = validationErrorFactory();
+					() => {
+						const bodyError = [
+							{
+								field: 'field',
+								message: '"field" has an error'
+							}
+						];
+						const error = validationErrorFactory({
+							body: bodyError as []
+						});
 						const mockResponse = responseFactory();
 						jest.spyOn(mockResponse, 'json');
-		
+
 						const response = validationHandling(
-							error, 
+							error,
 							jest.mocked<Request>({} as Request),
 							mockResponse,
 							jest.fn()
 						);
-		
-						expect(response.json).toBeCalledWith(error);
+
+						expect(response.json).toBeCalledWith(
+							{
+								title: error.message,
+								code: error.error,
+								errors: bodyError
+							}
+						);
 					}
 				);
 				it(
 					'should return http 400 bad request',
-					()=>{
+					() => {
 						const error = validationErrorFactory();
 						const mockResponse = responseFactory();
 						jest.spyOn(mockResponse, 'status');
-		
+
 						const response = validationHandling(
-							error, 
-							jest.mocked<Request>({} as Request), 
-							mockResponse, 
+							error,
+							jest.mocked<Request>({} as Request),
+							mockResponse,
 							jest.fn()
 						);
-		
+
 						expect(response.status).toBeCalledWith(400);
 					}
 				);
