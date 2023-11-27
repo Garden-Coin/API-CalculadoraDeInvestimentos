2a4c4f7a853754559e1ef3bf4055923a68cfece6
Franklin Bitencourt <franklin.bdantas@hotmail.com>
2023-11-26 22:46:45 -0300

    Altera estrutura do retorno do erro de validação

--- src/middlewares/validation-handling.ts
+++ src/middlewares/validation-handling.ts
@@ -5,8 +5,13 @@ import { ValidationError } from 'express-validation';
 const validationHandling = function (err: ValidationError, _req: Request, res: Response, _next: NextFunction){
 	return res.status(400).json({
 		title: err.message,
-		code: err.message,
-		// errors: err.details.body.
+		code: err.error,
+		errors: err.details.body?.map(
+			error => ({
+				field: error.message.split('"')[1],
+				message: error.message
+			})
+		)
 	});
 };
 
