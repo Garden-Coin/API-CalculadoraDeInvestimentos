9cc3f5b2c4b919784acaeec69ab1ac8de3fedaaa
Franklin Bitencourt <franklin.bdantas@hotmail.com>
2023-11-27 21:09:51 -0300

    Corrige if de erro de validação do errorHandling com

--- src/middlewares/error-handling.ts
+++ src/middlewares/error-handling.ts
@@ -1,12 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Request, Response, NextFunction } from 'express';
-import { ValidationError } from 'express-validation';
 
 const errorHandling = function (err: Error, _req: Request, res: Response, _next: NextFunction) {
-	if (err instanceof ValidationError) {
-		return res.status(400).json(err);
-	}
-
 	return res.status(500).json(
 		{
 			message: err.message
