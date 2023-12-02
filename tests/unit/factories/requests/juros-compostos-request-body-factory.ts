86ed636c132f925fdb9826203af6bf00431fa47f
Franklin Bitencourt <franklin.bdantas@hotmail.com>
2023-12-02 13:47:34 -0300

    Adicona teste da validação do campo periodType

--- tests/unit/factories/requests/juros-compostos-request-body-factory.ts
+++ tests/unit/factories/requests/juros-compostos-request-body-factory.ts
@@ -4,6 +4,7 @@ import ProfitabilityType from '@src/enums/profitability-type';
 
 type JurosCompostosRequestBodyFactoryProps = {
     period?: number,
+    periodType?: number,
     profitability?: number,
     profitabilityType?: number
     initialValue?: number   
@@ -12,6 +13,7 @@ type JurosCompostosRequestBodyFactoryProps = {
 export default function(body? :JurosCompostosRequestBodyFactoryProps): JurosCompostosRequestBody {
 	return {
 		period:             body?.period ?? faker.number.int({min: 1, max: 100}),
+		periodType:  		body?.profitabilityType ?? faker.helpers.enumValue(ProfitabilityType),
 		profitability:      body?.profitability ?? faker.number.float({min: 0.1, max: 1.0}),
 		profitabilityType:  body?.profitabilityType ?? faker.helpers.enumValue(ProfitabilityType),
 		initialValue:       body?.initialValue ?? faker.number.float({min: 1000, max: 10000})
