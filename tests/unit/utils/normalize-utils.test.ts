88f79f6e3d2b3d9b564e9bab9c54c11032b9c6c1
Franklin Bitencourt <franklin.bdantas@hotmail.com>
2023-12-02 14:05:10 -0300

    Remove normalizarRentabilidadeMensal

--- tests/unit/utils/normalize-utils.test.ts
+++ tests/unit/utils/normalize-utils.test.ts
@@ -1,37 +1,8 @@
-import ProfitabilityType from '@src/enums/profitability-type';
-import { calcularCrescimento, calcularJurosCompostosMensais, normalizarRentabilidadeMensal } from '@src/utils/normalize-utils';
+import { calcularCrescimento, calcularJurosCompostosMensais } from '@src/utils/normalize-utils';
 import { randomFloat } from '@tests/shared/utils/faker-utils';
 
 const profitabilityTestCaseArray = [0.001, 0.01, 0.05, 0.1, 0.15, 0.2, 0.5, 0.8];
 const periodTestCaseArray = [12, 36, 108, 360, 696, 1200];
-describe('normalizarRentabilidadeMensal', () => {
-	describe('Anual Profitability', () => {
-		it.each(profitabilityTestCaseArray)('should divide profitability of %s by 12.68', (profitability) => {
-			const { montlhyProfitability } = normalizarRentabilidadeMensal(profitability, 12, ProfitabilityType.Anual);
-
-			expect(montlhyProfitability).toBe(profitability / 12.68);
-		});
-
-		it.each(periodTestCaseArray)('should multiply period of %s by 12', (period) => {
-			const { monthlyPeriod } = normalizarRentabilidadeMensal(0.05, period, ProfitabilityType.Anual);
-
-			expect(monthlyPeriod).toBe(period * 12);
-		});
-	});
-	describe('Monthly Profitability', () => {
-		it.each(profitabilityTestCaseArray)('should not change profitability %s', (profitability) => {
-			const { montlhyProfitability } = normalizarRentabilidadeMensal(profitability, 12, ProfitabilityType.Monthly);
-
-			expect(montlhyProfitability).toBe(profitability);
-		});
-
-		it.each(periodTestCaseArray)('should not change period %s', (period) => {
-			const { monthlyPeriod } = normalizarRentabilidadeMensal(0.05, period, ProfitabilityType.Monthly);
-
-			expect(monthlyPeriod).toBe(period);
-		});
-	});
-});
 
 describe('calcularJurosCompostosMensais', () => {
 	describe('no initialValue', () => {
