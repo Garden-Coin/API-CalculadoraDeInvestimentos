0b6b7a4bf4c4e420f80ec58773ba87255d6b4324
Franklin Bitencourt <franklin.bdantas@hotmail.com>
2023-12-02 13:56:34 -0300

    Corrige JurosCompostosRequestBodyFactory

--- tests/unit/factories/requests/juros-compostos-request-body-factory.ts
+++ tests/unit/factories/requests/juros-compostos-request-body-factory.ts
@@ -13,7 +13,7 @@ type JurosCompostosRequestBodyFactoryProps = {
 export default function(body? :JurosCompostosRequestBodyFactoryProps): JurosCompostosRequestBody {
 	return {
 		period:             body?.period ?? faker.number.int({min: 1, max: 100}),
-		periodType:  		body?.profitabilityType ?? faker.helpers.enumValue(ProfitabilityType),
+		periodType:  		body?.periodType ?? faker.helpers.enumValue(ProfitabilityType),
 		profitability:      body?.profitability ?? faker.number.float({min: 0.1, max: 1.0}),
 		profitabilityType:  body?.profitabilityType ?? faker.helpers.enumValue(ProfitabilityType),
 		initialValue:       body?.initialValue ?? faker.number.float({min: 1000, max: 10000})
